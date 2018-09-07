6846ccb2f1470e9a0f10efdaaef99e83bb403bd0
Esteban Ramirez <esteban.ramirez.calvo@est.una.ac.cr>
2018-09-07 02:22:16 -0600

    make it better

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist/client'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/client/index.html'));
 });
 
 console.log('Console listening!');
